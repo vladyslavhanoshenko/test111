1d7dbbceaca078d0ba81d8c6fecbbf50175baf68
Vladyslav Hanoshenko1 <Vladyslav_Hanoshenko1@epam.com>
2019-03-12 18:20:38 +0200

    test

---
+++ HW3.1/HW3.1/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace HW3._1
+{
+
+    class Program
+    {
+        public static bool isStop = true;
+        public static bool isSorted = true;
+
+        public static void ShortList(int[] array)
+        {
+            if (array.Length >= 2)
+            {
+                int[] newArr = new int[2];
+                newArr[0] = array[0];
+                newArr[newArr.Length - 1] = array[array.Length - 1];
+                Console.WriteLine("Your short list is:");
+                Console.WriteLine($"{newArr[0]} {newArr[1]}");
+            }                    
+        }
+
+
+        public static void DescendOrder(ArrayList lst)
+        {
+            Console.WriteLine("Your sorted descending list is: ");
+            int count = 0;
+            int max = 0;
+            int min = 1;
+
+            foreach (var item in lst)
+            {
+                count++;
+            }
+
+            
+            int[] array = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = (int)lst[i];       
+            }
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                else if(array[i] < min)
+                {
+                    min = array[i];
+                }     
+            }
+          
+            while(isSorted){
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    int temp = 0;
+                    if(array[0] == max && array[array.Length-1] == min)
+                    {
+                        isSorted = false;
+                    }
+                    if (array[i] < array[i + 1])
+                    {
+                        temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+               
+            }
+
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            ShortList(array);
+           
+        }
+
+        public static void Start()
+        {
+            Console.WriteLine("Please enter int items to the list(enter 'stop' to finish:)");
+            ArrayList lst = new ArrayList();
+            int i = 0;
+
+
+
+            while (isStop)
+            {
+                try
+                {
+                    string temp = Console.ReadLine();
+                    if (temp == "stop")
+                    {
+                        isStop = false;
+                        break;
+
+                    }
+
+                    lst.Add(Convert.ToInt32(temp));
+                }
+                catch (System.FormatException e)
+                {
+                    Console.WriteLine("Entered value is not an int. Please enter a number: ");
+                    continue;
+
+                }
+            }
+            Console.WriteLine("Your list is:");
+
+            foreach (var a in lst)
+            {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine();
+
+            DescendOrder(lst);
+
+            Console.ReadLine();
+
+        }
+
+            static void Main(string[] args)
+            {
+            Start();
+            }
+        }
+    }
+
---
+++ HW3.2/HW3.2/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3._2
+{
+    class Program
+    {
+        public static bool isInputFinished = true;
+        public static void Start()
+        {
+
+            Console.WriteLine("Please enter students and their marks: (enter 'stop' to finish)");
+            Dictionary<string, int> students = new Dictionary<string, int>();
+
+            while (isInputFinished)
+            {
+                try
+                {
+
+
+                    string EnteredStr = Console.ReadLine();
+                    if (EnteredStr == "stop")
+                    {
+                        isInputFinished = false;
+                        break;
+                    }
+
+                    string[] surname_key = EnteredStr.Split(new char[] { ':' });
+                    
+                    string studentSurname = surname_key[0];
+                    if (isChar(studentSurname) == false)
+                    {
+                        Console.WriteLine("Input has wrong format! Desired format: 'Surname:Mark'");
+                        Console.WriteLine("Mark should be a number from 1 to 5'");
+                        Console.WriteLine("Please enter students and their marks: (enter 'stop' to finish)");
+                        continue;
+                    }
+                    int studentMark = Convert.ToInt32(surname_key[1]);
+                    if (studentMark < 1 || studentMark > 5)
+                    {
+                        Console.WriteLine("Mark should be a number from 1 to 5. Try again");
+                        continue;
+                    }
+                    students.Add(studentSurname, studentMark);
+
+
+                }
+                catch (System.IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Input has wrong format! Desired format: 'Surname:Mark'");
+                    Console.WriteLine("Mark should be a number from 1 to 5'");
+                    continue;
+                }
+            }
+
+            
+
+            DictionaryDisplay(students);
+            
+        }
+
+        public static void DictionaryDisplay(Dictionary<string, int> dic)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Please enter surname to see student's mark or a mark to see all students with it:");
+            Console.WriteLine("Or enter 'exit' to exit");
+            while (true)
+            {
+                try{
+
+                string temp = Console.ReadLine();
+                if (temp == "exit")
+                {
+                    isInputFinished = false;
+                    break;
+                }
+
+                if (isDigit(temp) == true)
+                {
+                    int count = 0;
+                    int mark = Convert.ToInt32(temp);
+                    Console.WriteLine($"Students with {mark} mark:");
+                    foreach (var item in dic)
+                    {
+                        if (item.Value == mark)
+                        {
+                            Console.Write($"{item.Key}" + " ");
+                        }
+                        else{
+                            count++;
+                            
+                            }
+                    }
+
+                    if(count==dic.Count){
+                        Console.WriteLine($"There are no students with {mark} mark");
+                    }
+
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"Student {temp} received a {dic[temp]}");
+                        StartString();
+                    
+                }
+                } catch(System.FormatException e2){
+                    Console.WriteLine("Input has wrong format");
+                    StartString();
+                    continue;
+                    
+                } catch(System.Collections.Generic.KeyNotFoundException e3){
+                    Console.WriteLine("Input has wrong format");
+                    StartString();
+                    continue;
+                }
+
+
+            }
+        }
+
+        public static void StartString()
+        {
+            Console.WriteLine("Please enter surname to see student's mark or a mark to see all students with it:");
+            Console.WriteLine("Or enter 'exit' to exit");
+            Console.WriteLine();
+
+        }
+
+        public static bool isDigit(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+
+        }
+
+        public static bool isChar(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Start();
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
